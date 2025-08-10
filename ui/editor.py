85aa6ca9ddf3664c466a8cd8ba7d248b824f8e79
Dev-Akuma <m.devasis.singh@gmail.com>
2025-08-11 00:59:41 +0530

    added inline suggestion and tab, enter acceptance of the suggestion.
    Bug : will need to use space to cancel the suggestion because enter accepts the suggestion and keeps accepting it so ... would need space to first stop suggestions and then press enter to get to the next line.

--- ui/editor.py
+++ ui/editor.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QPlainTextEdit, QCompleter
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QTextCursor
 from PyQt5.QtCore import Qt
 from ui.syntax_highlighter import SyntaxHighlighter
 
@@ -19,21 +19,34 @@ class ScriptEditor(QPlainTextEdit):
         self.completer = QCompleter(self.keywords + self.applications)
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.completer.setWidget(self)
+        self.completer.setCompletionMode(QCompleter.PopupCompletion)
+        self.completer.setWrapAround(False)
         self.completer.activated.connect(self.insert_completion)
 
     def insert_completion(self, completion):
         tc = self.textCursor()
-        extra = len(completion) - len(self.completer.completionPrefix())
-        tc.movePosition(tc.Left, tc.MoveAnchor, len(self.completer.completionPrefix()))
+        # Move cursor left to select current prefix and replace it
+        prefix_len = len(self.completer.completionPrefix())
+        tc.movePosition(QTextCursor.Left, QTextCursor.MoveAnchor, prefix_len)
+        tc.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor, prefix_len)
         tc.insertText(completion)
         self.setTextCursor(tc)
 
+    def text_under_cursor(self):
+        tc = self.textCursor()
+        tc.select(QTextCursor.WordUnderCursor)
+        return tc.selectedText()
+
     def keyPressEvent(self, event):
         if self.completer.popup().isVisible():
-            # Let completer handle navigation keys
-            if event.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Escape,
-                               Qt.Key_Tab, Qt.Key_Backtab):
-                event.ignore()
+            # Accept completion on Tab or Enter
+            if event.key() in (Qt.Key_Enter, Qt.Key_Return, Qt.Key_Tab):
+                self.insert_completion(self.completer.currentCompletion())
+                event.accept()
+                return
+            elif event.key() == Qt.Key_Escape:
+                self.completer.popup().hide()
+                event.accept()
                 return
 
         super().keyPressEvent(event)
@@ -43,16 +56,14 @@ class ScriptEditor(QPlainTextEdit):
             return
 
         eow = "~!@#$%^&*()+{}|:\"<>?,./;'[]\\-="  # end of word characters
-        has_modifier = (event.modifiers() != Qt.NoModifier) and not ctrl_or_shift
         completion_prefix = self.text_under_cursor()
 
-        if not has_modifier and completion_prefix != '' and (len(completion_prefix) > 0) and event.text()[-1] not in eow:
+        if (not ctrl_or_shift and completion_prefix != '' and
+            len(completion_prefix) > 0 and event.text() not in eow):
             self.completer.setCompletionPrefix(completion_prefix)
-            self.completer.complete()
+            cr = self.cursorRect()
+            cr.setWidth(self.completer.popup().sizeHintForColumn(0) +
+                        self.completer.popup().verticalScrollBar().sizeHint().width())
+            self.completer.complete(cr)
         else:
             self.completer.popup().hide()
-
-    def text_under_cursor(self):
-        tc = self.textCursor()
-        tc.select(tc.WordUnderCursor)
-        return tc.selectedText()
